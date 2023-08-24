cb122b7e1d21d970e17ee19f65ff2bb6d0f1476d
unknown <ishan.pitre@gmail.com>
2023-08-24 02:10:18 -0700

    currency-converter app

---
+++ main.js
@@ -0,0 +1,34 @@
+const populate = async (value, currency) => {
+  try {
+    let myStr;
+    const url = `https://api.currencyapi.com/v3/latest?apikey=cur_live_vLk2gCWX5gQNBxDcjVOlFHDBuPHzHyD5SZRC6MrI&base_currency${currency}`;
+    let response = await fetch(url);
+    let rJson = await response.json();
+    document.querySelector(".output").style.display = "block";
+    for (let key of Object.keys(rJson["data"])) {
+      myStr += `
+    <tr>
+    <td>${key}</td>
+    <td>${rJson["data"][key]["code"]}</td>
+    <td>${Math.round(rJson["data"][key]["value"] * value)}</td>
+  </tr>
+    `;
+    }
+
+    const tableBody = document.querySelector("tbody");
+    tableBody.innerHTML = myStr;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("button clicked");
+  const value = parseInt(
+    document.querySelector("input[name='quantity']").value
+  );
+  const currency = document.querySelector("select[name='currency']").value;
+  populate(value, currency);
+});
